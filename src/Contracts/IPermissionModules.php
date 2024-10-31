d08c997b4e95af0bc246a3ef975507da37b4e8cb
Arslan4xint <arslan.ahmad@xintsolutions.com>
2024-10-31 15:02:43 +0500

    v2

--- src/Contracts/IPermissionModules.php
+++ src/Contracts/IPermissionModules.php
@@ -4,43 +4,8 @@ namespace App\Contracts;
 
 interface IPermissionModules
 {
-    public const DASHBOARD_MODULE = 'dashboard_module';
-    public const CUSTOMER_MANAGEMENT_MODULE = 'customer_management_module';
-    public const LOV_MANAGEMENT_MODULE = 'lov_module';
-    public const DEPARTMENT_MODULE = 'department_module';
-    public const EMPLOYEE_MODULE = 'employee_module';
-    public const SOURCE_OF_REVENUE_MODULE = 'source_of_revenue_module';
-    public const PURPOSE_OF_FINANCE_MODULE = 'purpose_of_finance_module';
-    public const REPORTS_MODULE = 'reports_module';
-    public const PERMISSION_MODULE = 'permissions_module';
-    public const ROLE_MODULE = 'roles_module';
-    public const LOAN_MANAGEMENT_MODULE = 'loan_module';
-    public const PARTNER_MANAGEMENT_MODULE = 'partner_module';
-    public const PARTNER_ADMIN_MANAGEMENT_MODULE = 'partner_admin_module';
-    public const PRODUCT_MANAGEMENT_MODULE = 'product_management_module';
-    public const API_MANAGEMENT_MODULE = 'api_module';
-    public const SYSTEM_LOGS_MODULE = 'system_logs_module';
-    public const ACTIVITY_LOGS_MODULE = 'activity_logs_module';
-    public const LANDING_PAGE_MANAGEMENT_MODULE = 'landing_page_module';
-    public const LANDING_PAGE_ACTIONS_MODULE = 'landing_page_actions_module';
-    public const HOME_PAGE_MANAGEMENT_MODULE = 'home_page_module';
-    public const HOME_PAGE_ACTIONS_MODULE = 'home_page_actions_module';
-    public const SETTING_MODULE = 'setting_module';
-    public const DOCUMENT_MODULE = 'document_module';
-    public const PROCESSING_FEE_SLAB_MODULE = "processing_fee_slab_module";
-    public const APPLICATION_STEPS_MODULE = "application_steps_module";
-    public const TERMS_CONDITIONS_MODULE = "terms_conditions_module";
-    public const PRODUCT_VERIFICATION_METHODS_MODULE = "product_verification_methods_module";
-    public const FEE_SETTING_MODULE = "fee_setting_module";
-    public const API_REQUEST_DURATION_MODULE = "api_request_duration_module";
-    public const DEPARTMENT_PERMISSION_MODULE = "department_permission_module";
 
-    public const PRODUCT_SETTING_MODULE = 'product_setting_module';
-    public const LOAN_APPLICATION_MODULE = 'loan_application_module';
-    public const VIEW_LOAN_APPLICATION_MODULE = 'view_loan_application_module';
-    public const APPLICATION_BOARD_MODULE = 'application_board_module';
-    public const BOARD_ACTIONS_MODULE = 'board_actions_module';
-    public const LEAD_MODULE = 'lead_module';
-    public const CUSTOMER_MODULE = 'customer_module';
-    public const DASHBOARD_CARDS_MODULE = 'dashboard_cards_module';
+    public const USERS_MODULE = 'users_module';
+    public const POSTS_MODULE = 'posts_module';
+
 }
--- src/config/modules-permission.php
+++ src/config/modules-permission.php
@@ -1,72 +1,21 @@
 <?php
 
-use App\Contracts\IUserPermissions;
 use App\Contracts\IPermissionModules;
-
-
-
+use \App\Contracts\IUserPermissions;
 
 return [
-    IPermissionModules::DASHBOARD_MODULE => [
-        IUserPermissions::DASHBOARD_SHOW,
-        IUserPermissions::APPLICATION_CHART,
-        IUserPermissions::DEPARTMENT_CHART,
-        IUserPermissions::PARTNER_APPLICATION_CHART,
-        IPermissionModules::DASHBOARD_CARDS_MODULE => [
-            IUserPermissions::TOTAL_APPLIED_APPS_CARD,
-            IUserPermissions::TOTAL_INCOMPLETE_APPS_CARD,
-            IUserPermissions::TOTAL_IN_PROGRESS_APPS_CARD,
-            IUserPermissions::TOTAL_APPROVED_APPS_CARD,
-            IUserPermissions::TOTAL_REJECTED_APPS_CARD,
-            IUserPermissions::TOTAL_DISBURSED_AMOUNT_CARD,
-            IUserPermissions::TOTAL_CUSTOMERS_CARD,
-            IUserPermissions::TOTAL_PARTNERS_CARD,
-            IUserPermissions::TOTAL_COMMISSION_CARD
-        ]
+    IPermissionModules::USERS_MODULE => [
+        IUserPermissions::USERS_SHOW,
+        IUserPermissions::USERS_CREATE,
+        IUserPermissions::USERS_EDIT,
+        IUserPermissions::USERS_DELETE,
     ],
-    IPermissionModules::PRODUCT_MANAGEMENT_MODULE => [
-        IUserPermissions::PRODUCT_MANAGEMENT,
-        IUserPermissions::LIST_PRODUCT,
-        IUserPermissions::CREATE_PRODUCT,
-        IUserPermissions::EDIT_PRODUCT,
-        IUserPermissions::DELETE_PRODUCT,
-        IUserPermissions::UPDATE_PRODUCT_STATUS,
-        IUserPermissions::LIST_PRODUCT_ADMIN,
-        IUserPermissions::CREATE_PRODUCT_ADMIN,
-        IUserPermissions::EDIT_PRODUCT_ADMIN,
-        IUserPermissions::DELETE_PRODUCT_ADMIN,
-        IPermissionModules::DOCUMENT_MODULE => [
-            IUserPermissions::LIST_DOCUMENT,
-            IUserPermissions::CREATE_DOCUMENT,
-            IUserPermissions::EDIT_DOCUMENT,
-            IUserPermissions::DELETE_DOCUMENT,
-        ],
-        IPermissionModules::PRODUCT_SETTING_MODULE => [
-            IUserPermissions::LIST_PRODUCT_SETTING,
-            IPermissionModules::PROCESSING_FEE_SLAB_MODULE => [
-                IUserPermissions::LIST_PROCESSING_FEE_SLAB,
-                IUserPermissions::CREATE_PROCESSING_FEE_SLAB,
-                IUserPermissions::EDIT_PROCESSING_FEE_SLAB,
-                IUserPermissions::DELETE_PROCESSING_FEE_SLAB,
-            ],
-            IPermissionModules::PRODUCT_VERIFICATION_METHODS_MODULE => [
-                IUserPermissions::PRODUCT_VERIFICATION_METHODS,
-            ],
-            IPermissionModules::APPLICATION_STEPS_MODULE => [
-                IUserPermissions::APPLICATION_STEPS,
-            ],
-            IPermissionModules::TERMS_CONDITIONS_MODULE => [
-                IUserPermissions::TERMS_CONDITIONS,
-            ],
-            IPermissionModules::FEE_SETTING_MODULE => [
-                IUserPermissions::APPLICATION_FEE,
-            ],
-            IPermissionModules::API_REQUEST_DURATION_MODULE => [
-                IUserPermissions::API_REQUEST_DURATION,
-            ],
-            IPermissionModules::DEPARTMENT_PERMISSION_MODULE => [
-                IUserPermissions::DEPARTMENT_PERMISSIONS,
-            ],
-        ],
+
+    IPermissionModules::POSTS_MODULE => [
+        IUserPermissions::POSTS_SHOW,
+        IUserPermissions::POSTS_CREATE,
+        IUserPermissions::POSTS_EDIT,
+        IUserPermissions::POSTS_DELETE,
     ],
+
 ];
